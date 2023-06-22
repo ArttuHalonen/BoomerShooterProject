f77ec409c374c964433b22ff8a4afcdd3cd3de8a
ArttuHalonen <haloartt@hotmail.com>
2023-06-22 16:31:20 +0300

    put put

--- Assets/Assets/Script/Shooting/GunTypes/HitScanGun.cs
+++ Assets/Assets/Script/Shooting/GunTypes/HitScanGun.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections;
-using UnityEngine.ProBuilder.MeshOperations;
 
 public class HitScanGun : MonoBehaviour
 {
--- Assets/Assets/Script/Shooting/GunTypes/ProjectileGun.cs
+++ Assets/Assets/Script/Shooting/GunTypes/ProjectileGun.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class ProjectileGun : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform muzzle;
+    [SerializeField] private float ShootDelay;
+    [SerializeField] private EquipmentManager gunInfo;
 
     public GameObject projectile;
 
@@ -11,9 +14,44 @@ public class ProjectileGun : MonoBehaviour
     private void Start()
     {
         PlayerShoot.shootInput += Shoot;
+        PlayerShoot.reloadInput += StartReload;
     }
+    public void StartReload()
+    {
+        if (!gunInfo.gunReloading)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+    private IEnumerator Reload()
+    {
+        gunInfo.gunReloading = true;
+
+        yield return new WaitForSeconds(gunInfo.gunReloadTime);
+
+        gunInfo.gunCurrentAmmo = gunInfo.gunMaxAmmo;
+
+        gunInfo.gunReloading = false;
+    }
+    private bool CanShoot() => !gunInfo.gunReloading && ShootDelay > 1f / (gunInfo.gunFireRate / 60f);
     private void Shoot()
     {
-        Instantiate(projectile, muzzle.transform.position, muzzle.transform.rotation * Quaternion.Euler(0, 0, 0));
+        //check if mag is empty
+        if (gunInfo.gunCurrentAmmo > 0)
+        {
+            //check if CanShoot is True
+            if (CanShoot())
+            {
+                //spawn the bullit
+                Instantiate(projectile, muzzle.transform.position, muzzle.transform.rotation * Quaternion.Euler(0, 0, 0));
+            }
+            //update gunInfo
+            gunInfo.gunCurrentAmmo--;
+            ShootDelay = 0;
+        }
+    }
+    private void Update()
+    {
+        ShootDelay += Time.deltaTime;
     }
 }
